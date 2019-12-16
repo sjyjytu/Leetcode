92e194b9b2f6ebff2a4a00990bb3e9586ff26c65
sjyjtu <1332372004@qq.com>
2019-12-17 00:41:01 +0800

    332

--- QuestionBank/Reconstruct_Itinerary_332.h
+++ QuestionBank/Reconstruct_Itinerary_332.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 #include <queue>
 
 using namespace std;
@@ -23,7 +24,6 @@ public:
 		}
 		string src = "JFK";
 		vector<string> result = gogogo(src, "", path);
-		result.insert(result.begin(), src);
 		return result;
 	}
 
@@ -31,12 +31,13 @@ public:
 	vector<string> gogogo(string src, string back, 
 		unordered_map<string, priority_queue<string, vector<string>, cmp>>& path)
 	{
+		vector<string> result = { src };
 		//src一定不等于back，src的可能没有下家，可能下家是带分流的支路，可能下家就是back
 		if (path.count(src)==0 || path[src].size()==0)
 		{
-			return {};
+			return result;
 		}
-		vector<string> result;
+
 		while (path[src].size()==1)
 		{
 			string next = path[src].top();
@@ -55,7 +56,6 @@ public:
 			string next = path[src].top();
 			path[src].pop();
 			vector<string> one_special_roal = gogogo(next, src, path);
-			one_special_roal.insert(one_special_roal.begin(), next);
 			int size = one_special_roal.size();
 			if (one_special_roal[size -1] == src)
 			{
@@ -73,5 +73,38 @@ public:
 			result.insert(result.end(), should_add_to_back.begin(), should_add_to_back.end());
 		}
 		return result;
+	}
+
+	//看完答案我傻了，就tm是dfs的reverse
+	vector<string> findItinerary2(vector<vector<string>>& tickets) {
+		unordered_map<string, priority_queue<string, vector<string>, greater<string>>> path;
+		for (int i = 0; i < tickets.size(); i++)
+		{
+			path[tickets[i][0]].push(tickets[i][1]);
+		}
+		string src = "JFK";
+		vector<string> result;
+		dfs(src, result, path);
+		reverse(result.begin(), result.end());
+		return result;
+	}
+
+	void dfs(string node, vector<string>& result,
+		unordered_map<string, priority_queue<string, vector<string>, greater<string>>>& path)
+	{
+		if (path.count(node) == 0 || path[node].size() == 0)
+		{
+			result.push_back(node);
+			return;
+		}
+		string next;
+		while (!path[node].empty())
+		{
+			next = path[node].top();
+			path[node].pop();
+			dfs(next, result, path);
+		}
+		path.erase(node);
+		result.push_back(node);
 	}
 };
